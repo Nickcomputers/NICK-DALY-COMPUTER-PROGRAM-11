9e656613158f58c41b1e53c539462ec3b4c3d637
Nickcomputers <92175644+Nickcomputers@users.noreply.github.com>
2021-10-17 15:40:17 -0600

    Create Main.java
    Nick Daly Image Assignment

---
+++ image Nick Daly/src/com/company/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\t\t\t * \n\t\t\t*** ");
+        System.out.println("\t\t   ***** ");
+        System.out.println("\t\t ********* ");
+        System.out.println("\t\t***********");
+        System.out.println("\t\t    |||");
+        System.out.println("\t Merry  |||\tChristmas");
+        System.out.println("\t\t    |||");
+
+    }
+}
